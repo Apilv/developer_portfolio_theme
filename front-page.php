e9da75d40cadbbeb2460e6121bbe2fa352d58ebc
Andrius Pilvelis <andrius.pilv@gmail.com>
2020-10-15 15:18:25 +0300

    enqued all resources

--- front-page.php
+++ front-page.php
@@ -3,13 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="style.css">
-    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,400;0,500;0,600;0,700;1,400&display=swap"
-        rel="stylesheet">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
-        integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    <title>Developer Resume Theme</title>
+    <title>Developer Portfolio Theme</title>
+    <?php wp_head(); ?>
 </head>
 <body>
     <div class="cover-bg" id="home">
@@ -154,19 +149,9 @@
     <footer>
         <p class="mb-0 pt-2 text-white text-center"> &copy; 2020 Andrius Pilvelis</p>
     </footer>
-    <script src="https://kit.fontawesome.com/23d71ddeb9.js" crossorigin="anonymous"></script>
-  <script
-  src="https://code.jquery.com/jquery-3.5.1.min.js"
-  integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
-  crossorigin="anonymous"></script>
-   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
-   integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous">
-   </script>
-   <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+    <?php wp_footer() ?>
    <script>
   AOS.init();
 </script>
-   <script src="jquery.js"></script>
-   <script src="app.js"></script>
 </body>
 </html>
--- index.php
+++ index.php
@@ -6,5 +6,6 @@
     <title>Developer Theme</title>
 </head>
 <body>
+    <h1>Hello World</h1>
 </body>
 </html>
